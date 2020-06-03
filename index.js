2634db4b3652ce4256097ab1c31feaade7e9dadc
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-06-03 10:36:38 -0400

    middleware for routes started

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const express = require('express');
 const app = express();
 const routes = require("./routes");
 
+app.use("/roman-emperors", routes){
+    
+}
+
 app.listen(3001, ()=> console.log("listening"));
