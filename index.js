b53c2a5cd6e8ceaebbccee766046d650f7a85c6b
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-06-04 09:45:42 -0400

    fixed error in indexjs

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const routes = require("./routes");
 
-app.use("/roman-emperors", routes){
-    
-}
+app.use("/roman-emperors", routes);
 
 app.listen(3001, ()=> console.log("listening"));
