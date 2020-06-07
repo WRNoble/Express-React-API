a63678db6233fe79ed1dba83f7e9262dab2a4440
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-06-07 08:46:11 -0400

    updated seed file

--- db/seed.js
+++ db/seed.js
@@ -1,23 +1,12 @@
-const data = require('./data.json');
 const Emperor = require('../models/emperor-schema.js');
+const emperorData = require('');
 
-const emperorData = data.map(item => {
-    const emperor = {}
-    romanEmperor.name = item.name
-    romanEmperor.birth = item.birth
-    romanEmperor.birthProvidence = item.birthProvidence
-    romanEmperor.rise = item.rise 
-    romanEmperor.reignStart = item.reignStart
-    romanEmperor.dynasty = item.dynasty
-    romanEmperor.causeOfDeath = item.causeOfDeath
-    return RomanEmperor
-})
-
-RomanEmperor.deleteMany({})
-    .then(() => {
-        RomanEmperor.create(data)
-            .then(emperors => {
-                console.log(emperors);
-                process.exit();
-            });
+Emperor.deleteMany({});
+Emperor.create(emperorData)
+    .then(emperors => {
+        console.log(emperors)
+    })
+    .catch(err => {
+        console.log(err)
     });
+
