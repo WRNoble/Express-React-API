02f49f45e7699be8224e8d1be5ebc61aa5adadfd
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-06-09 12:20:30 -0400

    deploying

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,21 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/emperors", {useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise;
+
+let mongoURI = "";
+if (process.env.NODE_ENV === "production") {
+  mongoURI = process.env.DB_URL;
+} else {
+  mongoURI = "mongodb://localhost/emperors";
+}
+
+mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true })
     .then(instance => {
         console.log(`Connected to db: ${instance.connections[0].name}`)
     })
     .catch(err => console.log("Sorry, we failed to connect properly", err));
+
+
 
 module.exports = mongoose;
 
--- index.js
+++ index.js
@@ -25,4 +25,10 @@ app.use(function(err, req, res){
     })
 });
 
+app.set("port", process.env.PORT || 8080);
+
+app.listen(app.get("port"), () => {
+  console.log(`✅ PORT: ${app.get("port")} 🌟`);
+});
+
 app.listen(4000, ()=> console.log("listening on port 4000"));
