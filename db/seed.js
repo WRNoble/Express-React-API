f28ce7ccafd7718132c437e076c2ff973dffedb8
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-07-20 15:35:01 -0400

    attempting small modification to Augustus' birth year

--- db/seed.js
+++ db/seed.js
@@ -1,19 +1,16 @@
-const Emperor = require('../models/emperor-schema.js');
-const data = require('./data.json');
+const Emperor = require("../models/emperor-schema.js");
+const data = require("./data.json");
 
-Emperor.deleteMany({}, function(){
-    data.records.forEach(function(record){
-        Emperor.create({
-            name: record.fields.name,
-            birth: record.fields.birth,
-            birthProvidence: record.fields.birth_prv,
-            rise: record.fields.rise,
-            reignStart: record.fields.reign_start,
-            dynasty: record.fields.dynasty,
-            causeOfDeath: record.fields.cause
-        })
+Emperor.deleteMany({}, function () {
+  data.records.forEach(function (record) {
+    Emperor.create({
+      name: record.fields.name,
+      birth: record.fields.birth,
+      birthProvidence: record.fields.birth_prv,
+      rise: record.fields.rise,
+      reignStart: record.fields.reign_start,
+      dynasty: record.fields.dynasty,
+      causeOfDeath: record.fields.cause,
     });
+  });
 });
-
-
-
--- models/emperor-schema.js
+++ models/emperor-schema.js
@@ -2,13 +2,13 @@ const mongoose = require("../db/connection");
 const Schema = mongoose.Schema;
 
 const Emperor = new Schema({
-    name: String,
-    birth: String,
-    birthProvidence: String,
-    rise: String,
-    reignStart: String,
-    dynasty: String,
-    causeOfDeath: String
+  name: String,
+  birth: String,
+  birthProvidence: String,
+  rise: String,
+  reignStart: String,
+  dynasty: String,
+  causeOfDeath: String,
 });
 
 module.exports = mongoose.model("Emperor", Emperor);
